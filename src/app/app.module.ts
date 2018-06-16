ed38a16355f3746d328bc09e48061117bea58fc2
Katerina <sk.katherine@gmail.com>
2018-06-16 17:07:39 +0300

    the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ScriptLoadService } from './script-load.service';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+
+const config = {
+    apiKey: "AIzaSyC3X9Wdp9jgpOR2nCVhe4a8QV3nsns9Ie8",
+    authDomain: "map-psybercity.firebaseapp.com",
+    databaseURL: "https://map-psybercity.firebaseio.com",
+    projectId: "map-psybercity",
+    storageBucket: "map-psybercity.appspot.com",
+    messagingSenderId: "114738124484"
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [ScriptLoadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,65 @@
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { ScriptLoadService } from '../script-load.service';
+import { Marker } from '../marker';
+import { FirebaseApp } from 'angularfire2';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const your_API_key = 'AIzaSyAwVnwE1bEZf_Bkk_pSkGM0XlBSXJocVUY';
+const url = 'https://maps.googleapis.com/maps/api/js?key=' + your_API_key;
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements AfterViewInit {
+
+  markersRef: AngularFireList<any>;
+  markers: Observable<Marker[]>;
+  markersCh: Observable<Marker[]>;
+
+  @ViewChild('mapElement') mapElm: ElementRef;
+
+  constructor(private load: ScriptLoadService, private db: AngularFireDatabase) {
+
+    this.markers = this.db.list('/markers').valueChanges();
+
+  }
+
+  ngAfterViewInit(): void {
+    this.load.loadScript(url, 'gmap', () => {
+      this.maps = window['google']['maps'];
+      console.log(this.maps);
+      const loc = new this.maps.LatLng(51.561638, -0.14);
+      this.map = new this.maps.Map(this.mapElm.nativeElement, {
+        zoom: 11,
+        center: loc,
+        scrollwheel: true,
+        panControl: false,
+        mapTypeControl: false,
+        zoomControl: true,
+        streetViewControl: false,
+        scaleControl: true,
+        zoomControlOptions: {
+          style: this.maps.ZoomControlStyle.LARGE,
+          position: this.maps.ControlPosition.RIGHT_BOTTOM
+        }
+      });
+    }
+
+    this.markers.subscribe(x => {
+      console.log(x);
+      x.map(c => {
+        let marker = new this.maps.Marker({
+                position: c.position,
+                title: c.title,
+                map: this.map
+        })
+      })
+
+    });
+  }
+
+}
